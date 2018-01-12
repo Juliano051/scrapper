91ca160f79caea49380b7b02b6280ae50795b8b2
Juliano Silva CBTU <juliano051@gmail.com>
2018-01-12 14:01:56 -0200

    links xvs

--- scrapper_proxy.py
+++ scrapper_proxy.py
@@ -1,10 +1,16 @@
 import ssl
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 ssl._create_default_https_context = ssl._create_unverified_context
-html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
+# html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
+html = open("xvs.html", encoding="utf-8")
 bsObj = BeautifulSoup(html, "html.parser")
-for link in bsObj.findAll("a"):
- if 'href' in link.attrs:
-     print(link.attrs['href'])
+
+links = bsObj.findAll("a", {"href": re.compile("https://www.xvideos.com/video[0-9]")})
+print(len(links))
+for link in links:
+    print(link)
+#if 'href' in link.attrs:
+#    print(link.attrs['href'])
