c885dad4514f9da31a6043a2296bd8c29141bd25
Juliano Silva <juliano051@gmail.com>
2018-01-14 14:33:28 -0300

    14-01-2018

--- xvs_scrap_page.py
+++ xvs_scrap_page.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import re
 
 ssl._create_default_https_context = ssl._create_unverified_context
-html = urlopen("https://www.xvideos.com/tags/")
+html = urlopen("https://www.xvideos.com/tags/alone")
 # html = open("xvs.html", encoding="utf-8")
 bsObj = BeautifulSoup(html, "html.parser")
 
@@ -12,5 +12,5 @@ bsObj = BeautifulSoup(html, "html.parser")
 links = bsObj.findAll('a')
 #
 print(len(links))
-for link in links:
+for link in links
    print(link)
