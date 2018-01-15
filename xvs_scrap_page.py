3fd72d024294b56350e07ca355e34829e0264f19
Juliano Silva <juliano051@gmail.com>
2018-01-14 22:26:08 -0300

    14-01-18

--- xvs_scrap_page.py
+++ xvs_scrap_page.py
@@ -8,9 +8,15 @@ html = urlopen("https://www.xvideos.com/tags/alone")
 # html = open("xvs.html", encoding="utf-8")
 bsObj = BeautifulSoup(html, "html.parser")
 
+links = bsObj.findAll("div", {"class": "thumb-block"})
 # links = bsObj.findAll("a", {"href": re.compile("https://www.xs.com/video[0-9]")})
-links = bsObj.findAll('a')
-#
-print(len(links))
-for link in links
-   print(link)
+
+print(links[23].prettify())
+
+for link in links:
+    print(link.find("a", {"href": re.compile("/video[0-9]"), "title": re.compile("[A*-z*]")}).attrs["title"])
+    print("www.xvs.com/"+link.find("a", {"href": re.compile("/video[0-9]"), "title": re.compile("[A*-z*]")}).attrs["href"])
+    # print(link.find("span", {"class": "mobile-hide"}).get_text())
+    print(link.find("span", string=re.compile("[A*-z*]")).get_text())
+    print(link.find("img", {"data-src": re.compile("[A*-z*]")}).attrs["data-src"])
+    print("-"*20)
