71a8ae97d997d17187efe1c0bc469a6934fdae1e
Juliano Silva <juliano051@gmail.com>
2018-01-21 17:19:28 -0300

    21-01-2018_casa

--- supermercado/bluesoft/bs_scrap_produto.py
+++ supermercado/bluesoft/bs_scrap_produto.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 import ssl
 import requests
+import re
 from bs4 import BeautifulSoup
-
+import bs_scrap_salva_imagem
 
 ssl._create_default_https_context = ssl._create_unverified_context
 s = requests.session()
-
 
 def scrap_produto(link):
     # html = s.get("https://cosmos.bluesoft.com.br/produtos/"+str(ean_gtin))
@@ -15,17 +15,18 @@ def scrap_produto(link):
     bsObj = BeautifulSoup(html, "html.parser")
 
     # Buscar o link da imagem do produto
+    nome_prduto = bsObj.find( "meta", {"property": "og:title"})
+    cest_produto = bsObj.find("span", {"class": "description cest-name"})
+    ncm_produto = bsObj.find("a", {"title": "Mais produtos da NCM"}).findChild()
     imagem_produto = bsObj.find("div", {"class": "product-thumbnail"}).findChild().attrs["src"]  # localiza o link da imagem do produto
-
     lista_ean_gtin = bsObj.find("table", {"class": "table-striped"}).find("tbody").findAll("tr")  # localiza o link da imagem do produto
-
-    # ncm_produto = bsObj.find("h5", {"class": "description"}).find("p", {"class": "barcode"}).nextSibling
-
     info_produto = bsObj.find("dl", {"class": "dl-horizontal"}).findChildren()
 
-
+    print("Produto: "+nome_prduto.attrs["content"])
+    print("CEST: "+cest_produto.text[1:9])
+    print("NCM: "+str(ncm_produto.text[1:-1]))
     print(imagem_produto)
-
+    bs_scrap_salva_imagem.salva_imagem(imagem_produto, str(ncm_produto.text[1:-1]))
     print(info_produto[0].text[1:-1], info_produto[1].text[1:-1])
     print(info_produto[2].text[1:-1], info_produto[3].text[1:-1])
     print(info_produto[4].text[1:-1], info_produto[5].text[1:-1])
---
+++ supermercado/bluesoft/bs_scrap_salva_imagem.py
@@ -0,0 +1,30 @@
+# coding = utf-8
+'''
+Recebe como argumentos a url da imagem de um produto da internet e o codigo NCM no formato XXXX.XX.XX
+Sera criada uma pasta com o NCM do produto e a imagem será salva em seu interior
+O nome do arquivo da imagem é o próprio codigo GTIN
+'''
+import ssl
+import requests
+import os
+
+ssl._create_default_https_context = ssl._create_unverified_context
+s = requests.session()
+
+def salva_imagem(url, ncm):
+    pasta = '.\\' + ncm + '\\'
+    if not os.path.exists( pasta ):
+        os.makedirs( pasta )
+    else:
+        print( "Pasta ja existe" )
+
+    with open( pasta+url[-13:] + ".jpg", 'wb' ) as handle:
+        response = s.get(url, stream=True)
+        if not response.ok:
+            print( response )
+        for block in response.iter_content( 1024 ):
+            if not block:
+                break
+            handle.write( block )
+
+# salva_imagem("https://cdn-cosmos.bluesoft.com.br/products/7891000101506", "ncm_ficticio")
