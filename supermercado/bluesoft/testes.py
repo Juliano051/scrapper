3b68128c86b10d2877261ad920c7f64a61ffd47e
Juliano Silva <juliano051@gmail.com>
2018-01-21 00:53:09 -0300

    21-01-2018_casa

--- supermercado/bluesoft/testes.py
+++ supermercado/bluesoft/testes.py
@@ -1,8 +1,17 @@
+# coding = utf-8
+import ssl
+import requests
 import os
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+s = requests.session()
+
+foto = s.get("https://cdn-cosmos.bluesoft.com.br/products/7891000101506")
 
 newpath = './temp/'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-
 else:
     print("Pasta ja existe")
+foto.ret
